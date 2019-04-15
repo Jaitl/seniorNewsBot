aa7a13f0bb844d2b051d1400d1b8da9adcdf7734
Jaitl <jaitl@outlook.com>
2019-04-15 22:33:49 +0300

    NewsStorageActor

--- src/main/scala/com/github/jaitl/seniornews/aggregator/ChannelActor.scala
+++ src/main/scala/com/github/jaitl/seniornews/aggregator/ChannelActor.scala
@@ -4,7 +4,7 @@ import akka.actor.typed.ActorRef
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
 import com.github.jaitl.seniornews.models.ChannelInfo
-import com.github.jaitl.seniornews.sender.NewsSender.SenderMessage
+import com.github.jaitl.seniornews.storage.NewsStorageActor.StorageMessage
 
 import scala.util.Failure
 import scala.util.Success
@@ -21,11 +21,11 @@ object ChannelActor {
   def channel(
       reader: ChannelReader,
       channelInfo: ChannelInfo,
-      sender: ActorRef[SenderMessage]
+      sender: ActorRef[StorageMessage]
   ): Behavior[ChannelMessage] = Behaviors.receive { (context, message) =>
     reader.readUrl(channelInfo.url) match {
       case Success(items) =>
-        sender ! SenderMessage.SendNews(items)
+        sender ! StorageMessage.SaveMessage(items)
       case Failure(ex) =>
         context.log.error(ex, "Fail to request news from channel")
     }
